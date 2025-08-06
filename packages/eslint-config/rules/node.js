3079f40e70813935e3bc821abc5b7f0ea1662490
Raj Patel <rajp@ripple.com>
2025-08-06 09:57:33 -0400

    turn of two rules

--- packages/eslint-config/rules/node.js
+++ packages/eslint-config/rules/node.js
@@ -190,6 +190,16 @@ module.exports = [
           ignores: [],
         },
       ],
+
+      // Disallow import declarations which import non-existent modules
+      // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-missing-import.md
+      // Already handled by import/no-unresolved
+      'node/no-missing-import': 'off',
+
+      // Enforce the style of file extensions in import declarations
+      // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/file-extension-in-import.md
+      // Already handled by import/extensions
+      'node/file-extension-in-import': 'off',
     },
   },
   {
