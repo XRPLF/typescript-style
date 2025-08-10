830f6d73ba4144273812e3d930621a50aa00651d
Raj Patel <rajp@ripple.com>
2025-08-10 06:38:28 -0400

    add react version detect setting

--- packages/eslint-config/rules/react.js
+++ packages/eslint-config/rules/react.js
@@ -17,6 +17,11 @@ module.exports = [
         jsx: true,
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
       'react/jsx-filename-extension': [
         'error',
